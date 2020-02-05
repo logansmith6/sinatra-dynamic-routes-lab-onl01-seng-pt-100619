8e548afe812f95697c612cdd0ef8bc104202066f
Logan Smith <logsmith06@gmail.com>
2020-02-05 23:53:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ class App < Sinatra::Base
   get '/reversename/:name' do
       @user_name = params[:name]
       "#{@user_name.reverse}"
-
   end
+
+  get '/square/:number' do 
+    @number = params[:number]
+    "#{@number * @number}"
+  end 
 
 end
