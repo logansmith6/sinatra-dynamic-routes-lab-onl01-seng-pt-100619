192e1d67ca1e53a42fb63315e86edd7db34b77a4
Logan Smith <logsmith06@gmail.com>
2020-02-06 00:07:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,17 @@ class App < Sinatra::Base
     @number = params[:number]
     @phrase = params[:phrase]
     @x = @number.to_i
-
-
      "#{@phrase * @x}"
   end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1 @word2 @word3 @word4 @word5}"
+  end 
 
 end
